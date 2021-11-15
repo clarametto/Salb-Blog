109a98969bae5144341e2ebd6f4684b45dd71e0a
clara chepngeno metto <clara.metto@student.moringaschool.com>
2021-11-15 11:34:29 +0300

    create blog,new blog and blog update in views

--- app/main/views.py
+++ app/main/views.py
@@ -47,3 +47,49 @@ def updateprofile(name):
         form.email.data = current_user.email
         form.bio.data = current_user.bio
     return render_template('profile/update.html', user = user, form =form)
+
+@main.route('/new_post', methods=['POST','GET'])
+@login_required
+def new_blog():
+    subscribers = Subscriber.query.all()
+    form = CreateBlog()
+    if form.validate_on_submit():
+        title = form.title.data
+        description = form.description.data
+        content = form.content.data
+        user_id =  current_user._get_current_object().id
+        blog = Blog(title=title,description = description, content=content,user_id=user_id)
+        blog.save()
+        for subscriber in subscribers:
+            mail_message("New Blog Post","email/new_blog",subscriber.email,blog=blog)
+        return redirect(url_for('main.index'))
+    return render_template('post.html', form = form)
+
+@main.route('/blog/<id>')
+@login_required
+def blog(id):
+    comments = Comment.query.filter_by(blog_id=id).all()
+    blog = Blog.query.get(id)
+    return render_template('blog_page.html',blog=blog,comments=comments)
+    
+
+@main.route('/blog/<blog_id>/update', methods = ['GET','POST'])
+@login_required
+def updateblog(blog_id):
+    blog = Blog.query.get(blog_id)
+    if blog.user != current_user:
+        abort(403)
+    form = CreateBlog()
+    if form.validate_on_submit():
+        blog.title = form.title.data
+        blog.description = form.description.data
+        blog.content = form.content.data
+        db.session.commit()
+        return redirect(url_for('main.blog',id = blog.id)) 
+    if request.method == 'GET':
+        form.title.data = blog.title
+        form.description.data = blog.description
+        form.content.data = blog.content
+    return render_template('edit_blog.html', form = form)
+
+
