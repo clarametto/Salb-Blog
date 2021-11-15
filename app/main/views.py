db5a4bd3094fefd58233ecee08ffc36453c94475
clara chepngeno metto <clara.metto@student.moringaschool.com>
2021-11-15 11:32:43 +0300

    create profile and profile update in views

---
+++ app/main/views.py
@@ -0,0 +1,49 @@
+from flask import render_template, redirect,url_for,abort,request
+from . import main
+from app.requests import get_quote
+from flask_login import login_required,current_user
+from ..models import User,Blog,Comment,Subscriber
+from ..import db, photos
+import secrets
+import os
+from PIL import Image
+from .forms import UpdateProfile,CreateBlog
+from ..email import mail_message
+
+
+
+#Views
+@main.route('/')
+def index():
+    quote = get_quote()
+    blogs = Blog.query.order_by(Blog.time.desc())
+    return render_template('index.html', blogs=blogs,quote = quote)
+
+@main.route('/profile/<name>',methods = ['POST','GET'])
+@login_required
+def profile(name):
+    user = User.query.filter_by(username = name).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+
+    return render_template('profile/profile.html',user = user)
+
+@main.route('/user/<name>/updateprofile', methods = ['POST','GET'])
+@login_required
+def updateprofile(name):
+    user = User.query.filter_by(username = name).first()
+    form = UpdateProfile()
+    if form.validate_on_submit():
+        user.username = form.username.data
+        user.email = form.email.data
+        user.bio = form.bio.data
+        db.session.commit()
+        return redirect(url_for('main.profile',name=user.username,))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+        form.bio.data = current_user.bio
+    return render_template('profile/update.html', user = user, form =form)
